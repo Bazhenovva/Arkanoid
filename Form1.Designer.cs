9bf8b59de5a4996322bde50899373c169a34c060
79817 <bazhenova.8@mail.ru>
2026-03-01 23:06:04 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Arkanoid;
+
+partial class Form1
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.components = new System.ComponentModel.Container();
+        this.timer = new System.Windows.Forms.Timer(this.components);
+        this.SuspendLayout();
+        // 
+        // timer
+        // 
+        this.timer.Interval = 10;
+        this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+        // 
+        // Form1
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.BackColor = System.Drawing.Color.Black;
+        this.ClientSize = new System.Drawing.Size(1153, 708);
+        this.DoubleBuffered = true;
+        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+        this.MaximizeBox = false;
+        this.MinimizeBox = false;
+        this.Name = "Form1";
+        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        this.Text = "Arkanoid";
+        this.Load += new System.EventHandler(this.Form1_Load);
+        this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+        this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+        this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+        this.ResumeLayout(false);
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Timer timer;
+}
---
+++ Models/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Arkanoid.Models;
+
+internal class Paddle
+{
+    public Rectangle Rect { get; set; }
+
+    public Paddle(Rectangle rect)
+    {
+        Rect = rect;
+    }
+
+    public void SetPaddlePos(int x)
+    {
+        Rect = new Rectangle(x, Rect.Y, Rect.Width, Rect.Height);
+    }
+}
